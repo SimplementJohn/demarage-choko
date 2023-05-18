e14ef67a792c1d2f2d607b5addccc0a9979c324c
John <101429411+SimplementJohn@users.noreply.github.com>
2023-05-18 09:15:29 +0200

    Add files via upload

---
+++ Démarage Basique.cmd
@@ -0,0 +1,24 @@
+usoclient StartScan
+
+reg.exe ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v EnableLUA /t REG_DWORD /d 0 /f
+
+net accounts /maxpwage:unlimited
+
+copy "C:\Users\%username%\Desktop\Demarrage Basique\TeamViewerQS.exe" "C:\Users\Public\Desktop\TeamViewerQS.exe"
+
+@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+
+choco install powertoys
+choco install 7zip.install -y
+choco install googlechrome -y
+choco install firefox -y
+choco install adobereader -y
+
+usoclient startdownload
+
+choco install jre8 -y
+choco install vlc -y
+choco install office365business -y
+choco install libreoffice-fresh -y
+
+usoclient startinstall
